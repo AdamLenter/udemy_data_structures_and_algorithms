67a158288a568c79a64cddee058d8a60598f0b64
Adam <adamlenter1@gmail.com>
2022-11-18 09:34:28 -0500

    Created a singly link list mdethod.

---
+++ linked_lists.js
@@ -0,0 +1,41 @@
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        const node = new Node(val);
+        this.length += 1;
+
+        if(!this.head) {
+            this.head = node;
+            this.tail = node;
+        }
+        else { 
+            if(this.head === this.tail) {
+                this.head.next = node;
+            }
+            else {
+                this.tail.next = node;
+            }
+            this.tail = node;
+        }
+    }
+}
+
+const list = new SinglyLinkedList();
+list.push("Hello");
+list.push("my");
+list.push("name");
+list.push("is");
+list.push("Adam");
+console.log(list.tail);
