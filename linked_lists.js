f1dfeb202ee3f09efe3e247b6408b792ad097e82
Adam <adamlenter1@gmail.com>
2022-11-18 11:02:18 -0500

    Created an unshift method for linked lists.

--- linked_lists.js
+++ linked_lists.js
@@ -60,6 +60,21 @@ class SinglyLinkedList{
         }
         return oldHead;
     }
+
+    unshift(val) {
+        const newNode = new Node(val);
+
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    return this;
+    }
 }
 
 const list = new SinglyLinkedList();
