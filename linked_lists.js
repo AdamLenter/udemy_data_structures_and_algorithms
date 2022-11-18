4bc95738c44665b119f9cc085836e92f992e7ef3
Adam <adamlenter1@gmail.com>
2022-11-18 10:14:19 -0500

    Created a pop method for linked lists.

--- linked_lists.js
+++ linked_lists.js
@@ -25,6 +25,29 @@ class SinglyLinkedList{
             this.tail = node;
         }
     }
+
+    pop() {
+        if(!this.head) {
+            return undefined;
+        }
+
+        var newTail = this.head;
+        for(let i = 0; i < this.length - 2; i++) {
+            newTail = newTail.next;
+        }
+
+        const oldTail = this.tail;
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length = this.length - 1;
+
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return oldTail;
+    }
 }
 
 const list = new SinglyLinkedList();
@@ -33,4 +56,5 @@ list.push("my");
 list.push("name");
 list.push("is");
 list.push("Adam");
+list.pop();
 console.log(list);
