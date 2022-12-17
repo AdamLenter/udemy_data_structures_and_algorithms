ae47e9ae5249df2221f19864dbe3d5eef5535d26
Adam <adamlenter1@gmail.com>
2022-12-17 14:58:52 -0500

    Added new nodes and edges.

--- adjacency_list.js
+++ adjacency_list.js
@@ -38,9 +38,13 @@ const graph = new Graph();
 const tokyo = graph.addVertex("Tokyo");
 const detroit = graph.addVertex("Detroit");
 const boston = graph.addVertex("Boston");
+const miami = graph.addVertex("Miami");
+const london = graph.addVertex("London");
 const tb = graph.addEdge("Tokyo", "Boston");
 const td = graph.addEdge("Tokyo", "Detroit");
 const db = graph.addEdge("Detroit", "Boston");
-graph.removeVertex("Boston");
+const mb = graph.addEdge("Miami", "Boston");
+const mt = graph.addEdge("Miami", "Tokyo");
+const ld = graph.addEdge("London", "Detroit");
 
 console.log(graph);
