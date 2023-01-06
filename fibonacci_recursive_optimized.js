dcfe0d577d19765ccb3950a0842e28d9324f9d98
Adam <adamlenter1@gmail.com>
2023-01-06 09:39:00 -0500

    Refactored code to eliminate extra if/then statements.

--- fibonacci_recursive_optimized.js
+++ fibonacci_recursive_optimized.js
@@ -7,32 +7,15 @@ function fibonacci(n) {
             return 1;
         }
 
-        if(fibValues[n - 1]) {
-            //There is an existing value for n-1:
-            if(fibValues[n - 2]) {
-                //There is also a fib value for n - 2:
-                fibValues[n] = fibValues[n - 1] + fibValues[n - 2];
-            }
-            else {
-                //There is no n - 2
-                fibValues[n] = fibValues[n - 1] + fibonacciHelper(n - 2);
-            }
-        }
-        else {
-            //There is no existing value for n-1
-            if(fibValues[n - 2]) {
-                //There is an existing value for n-2
-                fibValues[n] = fibonacciHelper(n - 1) + fibValues[n - 2];
-            }
-            else {
-                //There is no value for n - 2
-                fibValues[n] = fibonacciHelper(n - 1) + fibonacciHelper(n - 2);
-            }
-        }
+        const value1 = fibValues[n - 1] ? fibValues[n - 1] : fibonacciHelper(n - 1);
+        const value2 = fibValues[n - 2] ? fibValues[n - 2] : fibonacciHelper(n - 2);
+
+        fibValues[n] = value1 + value2;
+        
         return fibValues[n];
         
     }
     return fibonacciHelper(n);
 }
 
-console.log(fibonacci(100));   
+console.log(fibonacci(7));   
